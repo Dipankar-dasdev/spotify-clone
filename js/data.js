c9aeb0f984a4e931624549bdf7325f0e32ee13e5
Dipankar-dasdev <das.dipankar.dev@gmail.com>
2026-07-19 21:10:06 +0530

    fix some naming bug

--- js/data.js
+++ js/data.js
@@ -2,202 +2,202 @@ const albums = [
   {
     title: "After Hours",
     artist: "The Weeknd",
-    image: "assets/images/after-hours.PNG"
+    image: "assets/images/after-hours.png",
   },
   {
     title: "Starboy",
     artist: "The Weeknd",
-    image: "assets/images/starboy.PNG"
+    image: "assets/images/starboy.png",
   },
   {
     title: "Future Nostalgia",
     artist: "Dua Lipa",
-    image: "assets/images/future-nostalgia.PNG"
+    image: "assets/images/future-nostalgia.png",
   },
   {
     title: "÷ (Divide)",
     artist: "Ed Sheeran",
-    image: "assets/images/divide.PNG"
+    image: "assets/images/divide.png",
   },
   {
     title: "Harry's House",
     artist: "Harry Styles",
-    image: "assets/images/harrys-house.PNG"
+    image: "assets/images/harrys-house.png",
   },
   {
     title: "SOS",
     artist: "SZA",
-    image: "assets/images/sos.PNG"
+    image: "assets/images/sos.png",
   },
   {
     title: "DAMN.",
     artist: "Kendrick Lamar",
-    image: "assets/images/damn.PNG"
+    image: "assets/images/damn.png",
   },
   {
     title: "Justice",
     artist: "Justin Bieber",
-    image: "assets/images/justice.PNG"
+    image: "assets/images/justice.png",
   },
   {
     title: "Midnights",
     artist: "Taylor Swift",
-    image: "assets/images/midnights.PNG"
+    image: "assets/images/midnights.png",
   },
   {
     title: "Planet Her",
     artist: "Doja Cat",
-    image: "assets/images/planet-her.PNG"
-  }
+    image: "assets/images/planet-her.png",
+  },
 ];
 
 const artists = [
-    {
-        name: "The Weeknd",
-        image: "assets/images/artists/the-weeknd.PNG"
-    },
-    {
-        name: "Drake",
-        image: "assets/images/artists/drake.JPEG"
-    },
-    {
-        name: "Taylor Swift",
-        image: "assets/images/artists/taylor-swift.PNG"
-    },
-    {
-        name: "Ed Sheeran",
-        image: "assets/images/artists/ed-sheeran.JPEG"
-    },
-    {
-        name: "Dua Lipa",
-        image: "assets/images/artists/dua-lipa.JPG"
-    },
-    {
-        name: "Arijit Singh",
-        image: "assets/images/artists/arijit-singh.JPG"
-    },
-    {
-        name: "Karan Aujla",
-        image: "assets/images/artists/karan-aujla.JPEG"
-    },
-    {
-        name: "AP Dhillon",
-        image: "assets/images/artists/ap-dhillon.JPG"
-    },
-    {
-        name: "Travis Scott",
-        image: "assets/images/artists/travis-scott.PNG"
-    },
-    {
-        name: "Post Malone",
-        image: "assets/images/artists/post-malone.JPEG"
-    }
+  {
+    name: "The Weeknd",
+    image: "assets/images/artists/the-weeknd.png",
+  },
+  {
+    name: "Drake",
+    image: "assets/images/artists/drake.jpeg",
+  },
+  {
+    name: "Taylor Swift",
+    image: "assets/images/artists/taylor-swift.png",
+  },
+  {
+    name: "Ed Sheeran",
+    image: "assets/images/artists/ed-sheeran.jpeg",
+  },
+  {
+    name: "Dua Lipa",
+    image: "assets/images/artists/dua-lipa.jpg",
+  },
+  {
+    name: "Arijit Singh",
+    image: "assets/images/artists/arijit-singh.jpg",
+  },
+  {
+    name: "Karan Aujla",
+    image: "assets/images/artists/karan-aujla.jpeg",
+  },
+  {
+    name: "AP Dhillon",
+    image: "assets/images/artists/ap-dhillon.jpg",
+  },
+  {
+    name: "Travis Scott",
+    image: "assets/images/artists/travis-scott.png",
+  },
+  {
+    name: "Post Malone",
+    image: "assets/images/artists/post-malone.jpeg",
+  },
 ];
 
 const trendingSongs = [
   {
     title: "Paint The Town Red",
     artist: "Doja Cat",
-    image: "assets/images/planet-her.PNG"
+    image: "assets/images/planet-her.png",
   },
   {
     title: "HUMBLE.",
     artist: "Kendrick Lamar",
-    image: "assets/images/damn.PNG"
+    image: "assets/images/damn.png",
   },
   {
     title: "Peaches",
     artist: "Justin Bieber",
-    image: "assets/images/justice.PNG"
+    image: "assets/images/justice.png",
   },
   {
     title: "Golden",
     artist: "Harry Styles",
-    image: "assets/images/harrys-house.PNG"
+    image: "assets/images/harrys-house.png",
   },
   {
     title: "Blinding Lights",
     artist: "The Weeknd",
-    image: "assets/images/after-hours.PNG"
+    image: "assets/images/after-hours.png",
   },
   {
     title: "Popular",
     artist: "The Weeknd",
-    image: "assets/images/artists/the-weeknd.PNG"
+    image: "assets/images/artists/the-weeknd.png",
   },
   {
     title: "Levitating",
     artist: "Dua Lipa",
-    image: "assets/images/future-nostalgia.PNG"
+    image: "assets/images/future-nostalgia.png",
   },
   {
     title: "Shape of You",
     artist: "Ed Sheeran",
-    image: "assets/images/artists/ed-sheeran.JPEG"
+    image: "assets/images/artists/ed-sheeran.jpeg",
   },
   {
     title: "Anti-Hero",
     artist: "Taylor Swift",
-    image: "assets/images/midnights.PNG"
+    image: "assets/images/midnights.png",
   },
   {
     title: "God's Plan",
     artist: "Drake",
-    image: "assets/images/artists/drake.JPEG"
-  }
+    image: "assets/images/artists/drake.jpeg",
+  },
 ];
 
 const recentlyPlayed = [
   {
     title: "Justice",
     artist: "Justin Bieber",
-    image: "assets/images/justice.PNG"
+    image: "assets/images/justice.png",
   },
   {
     title: "Midnights",
     artist: "Taylor Swift",
-    image: "assets/images/midnights.PNG"
+    image: "assets/images/midnights.png",
   },
   {
     title: "Drake",
     artist: "Artist",
-    image: "assets/images/artists/drake.JPEG"
+    image: "assets/images/artists/drake.jpeg",
   },
   {
     title: "Future Nostalgia",
     artist: "Dua Lipa",
-    image: "assets/images/future-nostalgia.PNG"
+    image: "assets/images/future-nostalgia.png",
   },
   {
     title: "Starboy",
     artist: "The Weeknd",
-    image: "assets/images/starboy.PNG"
+    image: "assets/images/starboy.png",
   },
   {
     title: "The Weeknd",
     artist: "Artist",
-    image: "assets/images/artists/the-weeknd.PNG"
+    image: "assets/images/artists/the-weeknd.png",
   },
   {
     title: "Ed Sheeran",
     artist: "Artist",
-    image: "assets/images/artists/ed-sheeran.JPEG"
+    image: "assets/images/artists/ed-sheeran.jpeg",
   },
   {
     title: "Post Malone",
     artist: "Artist",
-    image: "assets/images/artists/post-malone.JPEG"
+    image: "assets/images/artists/post-malone.jpeg",
   },
   {
     title: "SOS",
     artist: "SZA",
-    image: "assets/images/sos.PNG"
+    image: "assets/images/sos.png",
   },
   {
     title: "Travis Scott",
     artist: "Artist",
-    image: "assets/images/artists/travis-scott.PNG"
-  }
+    image: "assets/images/artists/travis-scott.png",
+  },
 ];
 
 // const sections = [
